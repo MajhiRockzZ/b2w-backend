bda8af564714ae9e0b3cd85ddaa5f89db67a8f0d
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-08-14 20:57:11 +0530

    feat: Create a Home Page WordPress Template

--- header.php
+++ header.php
@@ -67,10 +67,10 @@
                     <?php
                     wp_nav_menu(array(
 
-                        'theme_location' => 'primary',
-                        'container' => 'nav',
-                        'container_class' => 'navbar-collapse collapse',
-                        'menu_class' => 'nav navbar-nav navbar-right'
+                        'theme_location'      => 'primary',
+                        'container'           => 'nav',
+                        'container_class'     => 'navbar-collapse collapse',
+                        'menu_class'          => 'nav navbar-nav navbar-right'
 
                     ));
                     ?>
