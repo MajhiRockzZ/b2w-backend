56ea91b24e5c694238d09ef8ab4a4bbef2b144a2
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-08-19 22:01:47 +0530

    feat: Customize the Archive Template

--- archive.php
+++ archive.php
@@ -10,44 +10,55 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
 
-		<?php if ( have_posts() ) : ?>
+<?php if (have_posts()) : ?>
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+    <section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+        <?php
+        the_archive_title('<h1 class="page-title">', '</h1>');
+        the_archive_description('<h3><small class="archive-description">', '</small></h3>');
+        ?>
+    </section>
 
-			endwhile;
+    <div class="container">
+    <div id="primary" class="row">
+    <main id="content" class="col-sm-8">
 
-			the_posts_navigation();
+    <?php
+    /* Start the Loop */
+    while (have_posts()) :
+        the_post();
 
-		else :
+        /*
+         * Include the Post-Type-specific template for the content.
+         * If you want to override this in a child theme, then include a file
+         * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+         */
+        get_template_part('template-parts/content', get_post_type());
 
-			get_template_part( 'template-parts/content', 'none' );
+    endwhile;
 
-		endif;
-		?>
+    the_posts_navigation();
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+else :
+
+    get_template_part('template-parts/content', 'none');
+
+endif;
+?>
+    </main><!-- /#content.col-sm-8 -->
+
+
+    <!-- SIDEBAR
+    ===================================================== -->
+    <aside class="col-sm-4">
+        <?php get_sidebar(); ?>
+    </aside><!-- /.col-sm-4 -->
+
+    </div><!-- /#primary.row -->
+    </div><!-- /.container -->
 
 <?php
-get_sidebar();
 get_footer();
